70ebb3f767fedcdca89bac81be2a7dc9a4abe403
= <th.synee@gmail.com>
2013-03-06 10:29:44 +0800

    VSN - 2013030603

--- src/com/xysta/web/controller/MainController.java
+++ src/com/xysta/web/controller/MainController.java
@@ -1,15 +1,11 @@
 package com.xysta.web.controller;
 
-import com.jfinal.aop.Before;
 import com.jfinal.core.Controller;
-import com.xysta.web.interceptor.MainInterceptor;
 
 public class MainController extends Controller{
-
 
 	public void index(){
 		renderJsp("index.jsp");
 	}
-	
-	
+
 }
